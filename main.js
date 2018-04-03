cb164a1f61f7b0282bc47fe0464aef459e9cddc3
Gabriel T S Nakamura <gabriel.nakamura@yahoo.com.br>
2018-04-02 21:27:25 -0300

    Adição de addWindow.html e algumas modificações em main.js Implementação de um menu File, contendo Add item, Clear items e quit. Implementação de um menu DevTools, contendo uma função de mostrar as DevTools e reload da página. O item AddItem de File gera uma nova janela permintindo a entrada de dados (ainda não funcional).

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
+let AddWindow;
 
 // Listen for the app to be ready
 app.on('ready', function(){
@@ -16,4 +17,83 @@ app.on('ready', function(){
         protocol: 'file:',
         slashes: true
     }));
+    // Quit app when closed
+    mainWindow.on('closed', function(){
+        app.quit();
+    });
+
+    // Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    // Insert menu
+    Menu.setApplicationMenu(mainMenu);
 });
+
+// Handle create AddWindow
+function createAddWindow(){
+    // Create new window
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add Shopping List Item'
+    });
+    // Load the html file into the window
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    // Garbage collection handle
+    addWindow.on('close', function(){
+        addWindow = null;
+    });
+}
+
+// Create menu template
+const mainMenuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Add item',
+                click(){
+                    createAddWindow();
+                }
+            },
+            {
+                label: 'Clear items'
+            },
+            {
+                label: "Quit",
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+// if mac, add empty obejct to menu
+if(process.platform == 'darwin'){
+    mainMenuTemplate.unshift({});
+}
+
+// Add developer tools item if not in production
+if(process.env.NODE_ENM != 'production'){
+    mainMenuTemplate.push({
+        label: 'Developer tools',
+        submenu: [
+            {
+                label: 'Toggle DevTools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+                // Show DevTools on last clicked window
+                click(item, focusedWindow){
+                    focusedWindow.toggleDevTools();
+                }          
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    });
+}
